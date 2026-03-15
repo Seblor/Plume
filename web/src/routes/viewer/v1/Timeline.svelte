519f968257e0811a27e39a6021a2f3c197067899
Seblor <github@seblor.fr>
2026-03-15 16:53:46 +0100

    Formatting issues

--- web/src/routes/viewer/v1/Timeline.svelte
+++ web/src/routes/viewer/v1/Timeline.svelte
@@ -1,362 +1,365 @@
 <script lang="ts">
-	import { createEventDispatcher, tick, onDestroy, onMount } from "svelte";
-	import type { LogData } from "@PlumeTypes";
-	//@ts-ignore
-	import d3KitTimeline from "d3kit-timeline";
-	import {
-		extractVoiceStateChanges,
-		generateLabel,
-		getColorFromLabel,
-	} from "./utils/timeline";
-	import { uniqueId } from "lodash";
-	import {
-		allChartWidths,
-		maxWidth,
-		scrollOnCursorChange,
-		scrollX,
-	} from "./store/timelines";
-	import { getUserDataDemoFallback } from "./utils/getUserData";
-	import _ from "lodash";
-    import { getScrollParent } from "../../../utils/dom";
+  import { createEventDispatcher, tick, onDestroy, onMount } from "svelte";
+  import type { LogData } from "@PlumeTypes";
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  //@ts-ignore
+  import d3KitTimeline from "d3kit-timeline";
+  import {
+    extractVoiceStateChanges,
+    generateLabel,
+    getColorFromLabel,
+  } from "./utils/timeline";
+  import { uniqueId } from "lodash";
+  import {
+    allChartWidths,
+    maxWidth,
+    scrollOnCursorChange,
+    scrollX,
+  } from "./store/timelines";
+  import { getUserDataDemoFallback } from "./utils/getUserData";
+  import _ from "lodash";
+  import { getScrollParent } from "../../../utils/dom";
 
-	const dispatch = createEventDispatcher<{ change: number }>();
-	const dispatchClose = createEventDispatcher();
+  const dispatch = createEventDispatcher<{ change: number }>();
+  const dispatchClose = createEventDispatcher();
 
-	let graphWidth = 0;
-	let wrapperWidth: number;
+  let graphWidth = 0;
+  let wrapperWidth: number;
 
-	let chart: d3KitTimeline;
+  let chart: d3KitTimeline;
 
-	let timelineWrapper: HTMLDivElement;
-	let timeline: HTMLDivElement;
-	let dummyTimeline: HTMLDivElement;
-	let cursorElement: HTMLDivElement;
-	let root: HTMLDivElement;
+  let timelineWrapper: HTMLDivElement;
+  let timeline: HTMLDivElement;
+  let dummyTimeline: HTMLDivElement;
+  let cursorElement: HTMLDivElement;
+  let root: HTMLDivElement;
 
-	let dayStartDate = new Date();
-	let dayEndDate = new Date();
+  let dayStartDate = new Date();
+  let dayEndDate = new Date();
 
-	let cursorLeft = 0;
+  let cursorLeft = 0;
 
-	let uuid = uniqueId("timeline-");
+  let uuid = uniqueId("timeline-");
 
-	export let logsData: LogData;
-	export let patchFilter: string = "";
-	export let currentPatchIndex = 0;
+  export let logsData: LogData;
+  export let patchFilter = "";
+  export let currentPatchIndex = 0;
 
-	$: currentPatch = logsData.timedPatches[currentPatchIndex];
+  $: currentPatch = logsData.timedPatches[currentPatchIndex];
 
-	$: {
-		if (timelineWrapper) {
-			timelineWrapper.scrollLeft = $scrollX;
-		}
-	}
+  $: {
+    if (timelineWrapper) {
+      timelineWrapper.scrollLeft = $scrollX;
+    }
+  }
 
-	$: {
-		if (logsData.timedPatches.length > 0) {
-			const firstPatchDate = new Date(logsData.timedPatches[0].timestamp);
-			dayStartDate = firstPatchDate;
-			const lastPatchDate = new Date(
-				logsData.timedPatches[logsData.timedPatches.length - 1].timestamp
-			);
-			dayEndDate = new Date(lastPatchDate.getTime());
-		}
-	}
+  $: {
+    if (logsData.timedPatches.length > 0) {
+      const firstPatchDate = new Date(logsData.timedPatches[0].timestamp);
+      dayStartDate = firstPatchDate;
+      const lastPatchDate = new Date(
+        logsData.timedPatches[logsData.timedPatches.length - 1].timestamp,
+      );
+      dayEndDate = new Date(lastPatchDate.getTime());
+    }
+  }
 
-	$: {
-		if (currentPatch) {
-			updateCursorPos();
-		}
-	}
+  $: {
+    if (currentPatch) {
+      updateCursorPos();
+    }
+  }
 
-	$: filteredLogs = logsData.timedPatches.filter((log) => {
-		if (typeof patchFilter === "string") {
-			const memberId: string = patchFilter;
-			return (
-				log.patches.find((patch) => patch.path.includes(memberId)) !== undefined
-			);
-		} else {
-			return [];
-		}
-	});
+  $: filteredLogs = logsData.timedPatches.filter((log) => {
+    if (typeof patchFilter === "string") {
+      const memberId: string = patchFilter;
+      return (
+        log.patches.find((patch) => patch.path.includes(memberId)) !== undefined
+      );
+    } else {
+      return [];
+    }
+  });
 
-	$: data = filteredLogs.map((timedPatches) => {
-		const { voiceStateChangeLabel, patchesHandled } = extractVoiceStateChanges(
-			timedPatches.patches
-		);
-		const label = generateLabel(
-			timedPatches.patches.filter((patch) => !patchesHandled.includes(patch))
-		);
-		return {
-			time: new Date(timedPatches.timestamp),
-			patchIndex: logsData.timedPatches.indexOf(timedPatches) + 1,
-			name: voiceStateChangeLabel + label,
-		};
-	});
+  $: data = filteredLogs.map((timedPatches) => {
+    const { voiceStateChangeLabel, patchesHandled } = extractVoiceStateChanges(
+      timedPatches.patches,
+    );
+    const label = generateLabel(
+      timedPatches.patches.filter((patch) => !patchesHandled.includes(patch)),
+    );
+    return {
+      time: new Date(timedPatches.timestamp),
+      patchIndex: logsData.timedPatches.indexOf(timedPatches) + 1,
+      name: voiceStateChangeLabel + label,
+    };
+  });
 
-	$: {
-		filteredLogs;
-		tick().then(() => {
-			updateGraph();
-		});
-	}
+  $: {
+    filteredLogs;
+    tick().then(() => {
+      updateGraph();
+    });
+  }
 
-	$: {
-		$maxWidth;
-		updateGraph({ updateDummy: false });
-	}
+  $: {
+    $maxWidth;
+    updateGraph({ updateDummy: false });
+  }
 
-	function dispatchEvent(patchIndex: number) {
-		dispatch("change", patchIndex);
-	}
+  function dispatchEvent(patchIndex: number) {
+    dispatch("change", patchIndex);
+  }
 
-	function updateCursorPos() {
-		if (currentPatch === undefined) {
-			return;
-		}
-		const currentPatchDate = new Date(currentPatch.timestamp);
-		const percentageOfDay =
-			(currentPatchDate.getTime() - dayStartDate.getTime()) /
-			(dayEndDate.getTime() - dayStartDate.getTime());
+  function updateCursorPos() {
+    if (currentPatch === undefined) {
+      return;
+    }
+    const currentPatchDate = new Date(currentPatch.timestamp);
+    const percentageOfDay =
+      (currentPatchDate.getTime() - dayStartDate.getTime()) /
+      (dayEndDate.getTime() - dayStartDate.getTime());
 
-		cursorLeft =
-			(parseInt(timeline?.querySelector("svg")?.getAttribute("width") ?? "40") -
-				40) *
-			percentageOfDay;
+    cursorLeft =
+      (parseInt(timeline?.querySelector("svg")?.getAttribute("width") ?? "40") -
+        40) *
+      percentageOfDay;
 
-		tick().then(() => {
-			syncScrollWithSlider();
-		});
-	}
+    tick().then(() => {
+      syncScrollWithSlider();
+    });
+  }
 
-	$: {
-		$scrollOnCursorChange;
-		syncScrollWithSlider();
-	}
+  $: {
+    $scrollOnCursorChange;
+    syncScrollWithSlider();
+  }
 
-	function syncScrollWithSlider() {
-		if (root && cursorElement && $scrollOnCursorChange) {
-			// Scroll to the cursor element by cancel out vertical scrolling since there might by other cursord outside viewport
-			const scrollRoot = getScrollParent(root);
-			const beforeScroll = scrollRoot?.scrollTop || 0;
+  function syncScrollWithSlider() {
+    if (root && cursorElement && $scrollOnCursorChange) {
+      // Scroll to the cursor element by cancel out vertical scrolling since there might by other cursord outside viewport
+      const scrollRoot = getScrollParent(root);
+      const beforeScroll = scrollRoot?.scrollTop || 0;
 
-			cursorElement.scrollIntoView({
-				inline: "center",
-				block: "nearest",
-			});
+      cursorElement.scrollIntoView({
+        inline: "center",
+        block: "nearest",
+      });
 
-			if (scrollRoot && scrollRoot.scrollTop != undefined) {
-				scrollRoot.scrollTop = beforeScroll;
-			}
-		}
-	}
+      if (scrollRoot && scrollRoot.scrollTop != undefined) {
+        scrollRoot.scrollTop = beforeScroll;
+      }
+    }
+  }
 
-	function color5(d: any) {
-		return getColorFromLabel(d.name);
-	}
+  function color5(d: any) {
+    return getColorFromLabel(d.name);
+  }
 
-	/**
-	 * Updating the graph.
-	 * The dummy is used to get the actual graph's width since d3KitTimeline does not properly resize
-	 * @param updateDummy
-	 */
-	function updateGraph({
-		updateDummy = false,
-	}: { updateDummy?: boolean } = {}) {
-		if (!timeline || !dummyTimeline) {
-			return;
-		}
-		if (updateDummy === false) {
-			timeline.innerHTML = "";
-			dummyTimeline.innerHTML = "";
-			updateGraph({ updateDummy: true });
-		}
+  /**
+   * Updating the graph.
+   * The dummy is used to get the actual graph's width since d3KitTimeline does not properly resize
+   * @param updateDummy
+   */
+  function updateGraph({
+    updateDummy = false,
+  }: { updateDummy?: boolean } = {}) {
+    if (!timeline || !dummyTimeline) {
+      return;
+    }
+    if (updateDummy === false) {
+      timeline.innerHTML = "";
+      dummyTimeline.innerHTML = "";
+      updateGraph({ updateDummy: true });
+    }
 
-		let chartWidth =
-			graphWidth + 100 < wrapperWidth ? wrapperWidth - 100 : graphWidth + 200;
+    let chartWidth =
+      graphWidth + 100 < wrapperWidth ? wrapperWidth - 100 : graphWidth + 200;
 
-		if (updateDummy === false) {
-			$allChartWidths[uuid] = chartWidth;
+    if (updateDummy === false) {
+      $allChartWidths[uuid] = chartWidth;
 
-			chartWidth = $maxWidth;
-		}
+      chartWidth = $maxWidth;
+    }
 
-		const newChart = new d3KitTimeline(
-			`#${uuid}-${updateDummy ? "timeline-dummy-graph" : "timeline-graph"}`,
-			{
-				direction: "up",
-				initialWidth: chartWidth,
-				formatAxis: (axis: any) =>
-					axis.tickFormat((d: Date) => {
-						return `${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
-					}),
-				domain: [dayStartDate, dayEndDate],
-				margin: { left: 20, right: 20, top: 20, bottom: 30 },
-				textFn: (d: any) => d.name,
-				layerGap: 40,
-				dotColor: color5,
-				labelBgColor: color5,
-				linkColor: color5,
-				labella: {
-					minPos: 0,
-					maxPos: chartWidth - 100,
-					nodeSpacing: 10,
-					algorithm: "none",
-				},
-			}
-		);
+    const newChart = new d3KitTimeline(
+      `#${uuid}-${updateDummy ? "timeline-dummy-graph" : "timeline-graph"}`,
+      {
+        direction: "up",
+        initialWidth: chartWidth,
+        formatAxis: (axis: any) =>
+          axis.tickFormat((d: Date) => {
+            return `${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
+          }),
+        domain: [dayStartDate, dayEndDate],
+        margin: { left: 20, right: 20, top: 20, bottom: 30 },
+        textFn: (d: any) => d.name,
+        layerGap: 40,
+        dotColor: color5,
+        labelBgColor: color5,
+        linkColor: color5,
+        labella: {
+          minPos: 0,
+          maxPos: chartWidth - 100,
+          nodeSpacing: 10,
+          algorithm: "none",
+        },
+      },
+    );
 
-		newChart.on("labelClick", (element: { data: (typeof data)[0] }) => {
-			dispatchEvent(element.data.patchIndex);
-		});
+    newChart.on("labelClick", (element: { data: (typeof data)[0] }) => {
+      dispatchEvent(element.data.patchIndex);
+    });
 
-		newChart.data(data).updateDimensionNow().resizeToFit().visualize();
-		if (updateDummy) {
-			graphWidth =
-				newChart.svg.selectAll(".main-layer").node().getBoundingClientRect()
-					.width || graphWidth;
-		} else {
-			chart = newChart;
-			updateCursorPos();
-			timelineWrapper.scrollLeft = $scrollX;
-		}
-	}
+    newChart.data(data).updateDimensionNow().resizeToFit().visualize();
+    if (updateDummy) {
+      graphWidth =
+        newChart.svg.selectAll(".main-layer").node().getBoundingClientRect()
+          .width || graphWidth;
+    } else {
+      chart = newChart;
+      updateCursorPos();
+      timelineWrapper.scrollLeft = $scrollX;
+    }
+  }
 
-	function onTimelineScrollEvent(event: Event) {
-		$scrollX = (event.target as HTMLDivElement).scrollLeft;
-	}
+  function onTimelineScrollEvent(event: Event) {
+    $scrollX = (event.target as HTMLDivElement).scrollLeft;
+  }
 
-	onDestroy(() => {
-		delete $allChartWidths[uuid];
-		$allChartWidths = $allChartWidths;
-	});
+  onDestroy(() => {
+    delete $allChartWidths[uuid];
+    $allChartWidths = $allChartWidths;
+  });
 
-	onMount(() => {
-		updateCursorPos();
-	});
+  onMount(() => {
+    updateCursorPos();
+  });
 </script>
 
 <div class="timeline relative rounded-2xl" bind:this={root}>
-	<div
-		class="absolute top-0 left-0 cursor-pointer m-2"
-		on:click={() => dispatchClose("close")}
-		on:keypress={() => {}}
-		tabindex="0"
-		role="button"
-	>
-		<svg
-			class="w-4 h-4 text-gray-800 dark:text-white"
-			aria-hidden="true"
-			xmlns="http://www.w3.org/2000/svg"
-			fill="none"
-			viewBox="0 0 14 14"
-		>
-			<path
-				stroke="currentColor"
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				stroke-width="2"
-				d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-			/>
-		</svg>
-	</div>
-	<div class="w-40 flex justify-center align-center h-full">
-		{#await getUserDataDemoFallback(patchFilter)}
-			<span class="timeline-user">{patchFilter}</span>
-		{:then userData}
-			<div class="flex flex-col m-0 p-0 h-full w-full text-center">
-				<div
-					class="h-3/4 bg-no-repeat bg-center bg-cover"
-					style:background-image={"url(" + userData.avatar.link + ")"}
-				>
-					<!-- <img class="aspect-square" style="" src={userData.avatar.link} alt="user avatar" /> -->
-				</div>
-				<span class="timeline-user">{userData.global_name}</span>
-			</div>
-		{:catch error}
-			<span class="timeline-user">error: {patchFilter}</span>
-		{/await}
-	</div>
-	<div
-		class="timeline-wrapper"
-		bind:clientWidth={wrapperWidth}
-		bind:this={timelineWrapper}
-		on:scroll={onTimelineScrollEvent}
-	>
-		<div class="timeline-container" style:width="{graphWidth + 100}px">
-			<div
-				class="timeline-graph"
-				id={uuid + "-timeline-graph"}
-				bind:this={timeline}
-				bind:clientWidth={graphWidth}
-			/>
+  <div
+    class="absolute top-0 left-0 cursor-pointer m-2"
+    on:click={() => dispatchClose("close")}
+    on:keypress={() => {
+      void 0;
+    }}
+    tabindex="0"
+    role="button"
+  >
+    <svg
+      class="w-4 h-4 text-gray-800 dark:text-white"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 14 14"
+    >
+      <path
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+      />
+    </svg>
+  </div>
+  <div class="w-40 flex justify-center align-center h-full">
+    {#await getUserDataDemoFallback(patchFilter)}
+      <span class="timeline-user">{patchFilter}</span>
+    {:then userData}
+      <div class="flex flex-col m-0 p-0 h-full w-full text-center">
+        <div
+          class="h-3/4 bg-no-repeat bg-center bg-cover"
+          style:background-image={"url(" + userData.avatar.link + ")"}
+        >
+          <!-- <img class="aspect-square" style="" src={userData.avatar.link} alt="user avatar" /> -->
+        </div>
+        <span class="timeline-user">{userData.global_name}</span>
+      </div>
+    {:catch error}
+      <span class="timeline-user">error: {patchFilter}</span>
+    {/await}
+  </div>
+  <div
+    class="timeline-wrapper"
+    bind:clientWidth={wrapperWidth}
+    bind:this={timelineWrapper}
+    on:scroll={onTimelineScrollEvent}
+  >
+    <div class="timeline-container" style:width="{graphWidth + 100}px">
+      <div
+        class="timeline-graph"
+        id={uuid + "-timeline-graph"}
+        bind:this={timeline}
+        bind:clientWidth={graphWidth}
+      />
 
-			<!-- Values are fine tuned from various tests, no idea why d3kit-timeline fucks up dimensions so much -->
-			<div
-				class="cursor-wrapper cursor border border-primary-full"
-				bind:this={cursorElement}
-				style:left="{cursorLeft}px"
-			/>
+      <!-- Values are fine tuned from various tests, no idea why d3kit-timeline fucks up dimensions so much -->
+      <div
+        class="cursor-wrapper cursor border border-primary-full"
+        bind:this={cursorElement}
+        style:left="{cursorLeft}px"
+      />
 
-			<div
-				class="timeline-dummy-graph"
-				id={uuid + "-timeline-dummy-graph"}
-				bind:this={dummyTimeline}
-			/>
-		</div>
-	</div>
+      <div
+        class="timeline-dummy-graph"
+        id={uuid + "-timeline-dummy-graph"}
+        bind:this={dummyTimeline}
+      />
+    </div>
+  </div>
 </div>
 
 <style lang="scss">
-	.timeline {
-		width: 100%;
-		height: 125px;
-		overflow-y: hidden;
-		display: flex;
-		align-items: center;
-		background-color: rgba(255, 255, 255, 0.1);
-		// box-shadow: 0 0 10px 5px rgba(255, 255, 255, 0.1);
+  .timeline {
+    width: 100%;
+    height: 125px;
+    overflow-y: hidden;
+    display: flex;
+    align-items: center;
+    background-color: rgba(255, 255, 255, 0.1);
+    // box-shadow: 0 0 10px 5px rgba(255, 255, 255, 0.1);
 
-		&-user {
-			margin: 0.5rem;
-		}
+    &-user {
+      margin: 0.5rem;
+    }
 
-		&-wrapper {
-			margin-bottom: 1rem;
-			overflow-x: auto;
-			overflow-y: hidden;
-			flex: 1;
-		}
+    &-wrapper {
+      margin-bottom: 1rem;
+      overflow-x: auto;
+      overflow-y: hidden;
+      flex: 1;
+    }
 
-		&-container {
-			position: relative;
+    &-container {
+      position: relative;
 
-			.cursor {
-				position: absolute;
-				top: 0px;
-				height: 100%;
-				width: 0px;
-				// border: 1px solid #777;
-			}
+      .cursor {
+        position: absolute;
+        top: 0px;
+        height: 100%;
+        width: 0px;
+        // border: 1px solid #777;
+      }
 
-			.cursor-wrapper {
-				position: absolute;
-				pointer-events: none;
-				top: 0px;
-				height: 100%;
-				margin-left: 20px;
-				margin-right: 20px;
-			}
-		}
+      .cursor-wrapper {
+        position: absolute;
+        pointer-events: none;
+        top: 0px;
+        height: 100%;
+        margin-left: 20px;
+        margin-right: 20px;
+      }
+    }
 
-		&-graph {
-			display: inline-block;
-			// border: 1px solid #ccc;
-		}
+    &-graph {
+      display: inline-block;
+      // border: 1px solid #ccc;
+    }
 
-		&-dummy-graph {
-			visibility: hidden;
-			height: 0px;
-		}
-	}
+    &-dummy-graph {
+      visibility: hidden;
+      height: 0px;
+    }
+  }
 </style>
